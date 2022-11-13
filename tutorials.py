fae3d0a6474ce5a76da97253c9fb57d0b1b1adf8
NadKandil <ndine.kandil28@gmail.com>
2022-11-13 20:36:09 +0200

    message for tag

--- tutorials.py
+++ tutorials.py
@@ -2,10 +2,17 @@
 e = 765
 q = 56
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 y = 87
 a = 456
 =======
 p = 787
 v = 9876
 r = 46
+>>>>>>> Stashed changes
+=======
+p = 787
+v = 9876
+r = 46
+n= 44
 >>>>>>> Stashed changes
